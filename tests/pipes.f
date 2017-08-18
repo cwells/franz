f6e4865dd858f9e1de875c7c5ac3bf3041e958ad
Cliff Wells <cwells@focusvision.com>
2017-08-17 22:52:28 -0700

    finish pipe tests

--- tests/pipes.f
+++ tests/pipes.f
@@ -1,3 +1,10 @@
+#
+# pipes
+#
+
+print("Test pipes...")
+errors = 0
+
 a = fn (n: int)
     for i in 1 to n
         yield i
@@ -10,12 +17,11 @@ c = fn (iter: list)
     for i in iter
         yield i ^ 2
 
-value = [ a(4) -> b -> c ]
-print("value: {value}\n")
+pipes = [ a(4) -> b -> c ]
+nested = [ c(b(a(4))) ]
+known = [ 1, 16, 81, 256 ]
 
-value = [ c(b(a(4))) ]
-print("value: {value}\n")
+try assert pipes == nested rescue errors += 1
+try assert pipes == known rescue errors += 1
 
-assert value == [ 1, 16, 81, 256 ]
-
-# assert value == c(b(a(10)))
+print(if errors "Some tests failed ({errors}) errors\n" else "passed!\n")
