10e88d79d5a0d30bea7c28ba15748089f9c87b68
Cliff Wells <cwells@focusvision.com>
2017-08-18 15:06:52 -0700

    fibonacci

--- tests/fibonacci.f
+++ tests/fibonacci.f
@@ -8,18 +8,18 @@ errors = 0
 #
 # elegant, but extremely inefficient recursive algorithm
 #
-fibonacci-sequence = fn (start: int, end: int) {
+fibonacci-sequence = fn (n: int) {
     fibonacci-number = fn (nth: int)
         if nth <= 1
             nth
         else
             fibonacci-number(nth - 1) + fibonacci-number(nth - 2)
 
-    for i in start to end
+    for i in 1 to n
         yield fibonacci-number(i)
 }
 
-sequence = [ fibonacci-sequence(start: 1, end: 20) ]
+sequence = [ fibonacci-sequence(20) ]
 
 try assert sequence == [
     1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144,
@@ -33,7 +33,7 @@ rescue errors += 1
 fibonacci-sequence = fn (n: int) {
     a = b = 1
     for i in 3 to n + 2
-        (a, b) = (b, (yield a) + b)
+        (a, b) = (b, (yield a) + b);
 }
 
 sequence = [ fibonacci-sequence(20) ]
