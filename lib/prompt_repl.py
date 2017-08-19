02940e28a7b65c280dff5b0bd6b88c7e83076bd9
Cliff Wells <cwells@focusvision.com>
2017-08-18 23:22:47 -0700

    simplify prompt repl

--- lib/prompt_repl.py
+++ lib/prompt_repl.py
@@ -1,4 +1,5 @@
 import time
+import getpass
 
 from prompt_toolkit import prompt
 from prompt_toolkit.contrib.completers import WordCompleter
@@ -10,35 +11,19 @@ from prompt_toolkit.token import Token
 from lark.common import UnexpectedToken
 from lark.lexer import UnexpectedInput
 
-COMMANDS = [
-    "\c to clear buffer",
-    "\s to show buffer",
-    "\l to show last successful expression",
-    "\\t <expr> to time execution of <expr>",
-    "\T to show AST",
-    "\q (or ctrl+d) to quit"
-]
-
-def help():
-    for c in COMMANDS:
-        print("%s" % c)
-
-
-style = style_from_dict({
-    Token.Toolbar: '#ffffff bg:#333333',
-})
 
 def __repl(parser, interpreter):
-    code_block = []
-    last = ''
-    toolbar_value = ''
     history = InMemoryHistory()
+    toolbar_value = 'Hello, %s.' % getpass.getuser()
+
+    style = style_from_dict({
+        Token.Toolbar: '#ffffff bg:#333333',
+    })
 
     while True:
         name_completer = WordCompleter(sorted(interpreter.context))
-        start_eval_time = None
 
-        s = prompt('> ',
+        code = prompt('> ',
             multiline    = True,
             completer    = name_completer,
             history      = history,
@@ -47,43 +32,12 @@ def __repl(parser, interpreter):
             get_bottom_toolbar_tokens = lambda cli: [(Token.Toolbar, toolbar_value)]
         )
 
-        if not s.strip():
-            continue
-
-        if s == '\c':
-            code_block = []
-            continue
-        elif s in ('\h', '\?'):
-            help()
-            continue
-        elif s == '\l':
-            print(last, "\n")
-            continue
-        elif s == '\q':
-            print("Exiting.\n")
-            raise SystemExit
-        elif s == '\s':
-            print('\n'.join(code_block))
-            continue
-        elif s == '\T':
-            try:
-                print(ast.pretty())
-            except UnboundLocalError:
-                pass
-            continue
-
-        code_block.append(s)
-        s = '\n'.join(code_block)
+        if not code.strip(): continue
 
         try:
-            ast = parser.parse(s)
-        except UnexpectedToken:
+            ast = parser.parse(code)
+        except (UnexpectedToken, UnexpectedInput):
             continue
-        except UnexpectedInput:
-            code_block = code_block[:-1]
-            continue
-
-        code_block = []
 
         try:
             start_eval_time = time.time()
@@ -93,11 +47,10 @@ def __repl(parser, interpreter):
             continue
         else:
             toolbar_value = "time: {:0.4f} value: {}".format(time.time() - start_eval_time, retval)
-            last = s
 
 
 def repl(parser, interpreter):
-    print("Franz v0.0 (\h for help)\nPress [Alt]+[Enter] to complete an expression.")
+    print("Franz v0.0 Press [Alt]+[Enter] to evaluate an expression.")
     while True:
         try:
             __repl(parser, interpreter)
