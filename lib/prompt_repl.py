265289d8cb356436b2f45a6358514ca71975252b
Cliff Wells <cwells@focusvision.com>
2017-08-24 13:52:29 -0700

    allow for user-selected repl style

--- lib/prompt_repl.py
+++ lib/prompt_repl.py
@@ -7,28 +7,30 @@ from prompt_toolkit import prompt, AbortAction
 from prompt_toolkit.contrib.completers import WordCompleter
 from prompt_toolkit.history import FileHistory
 from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
-from prompt_toolkit.styles import style_from_dict
+from prompt_toolkit.styles import style_from_pygments
 from prompt_toolkit.token import Token
 from prompt_toolkit.layout.lexers import PygmentsLexer
 
 from lark.common import UnexpectedToken
 from lark.lexer import UnexpectedInput
 
-from .pygments import FranzLexer, ReplStyle
+from .pygments import FranzLexer, repl_styles
 
 
-ReplStyle.styles.update({
-    # menu
-    Token.Menu.Completions.Completion.Current: 'bg:#00aaaa #000000',
-    Token.Menu.Completions.Completion: 'bg:#008888 #ffffff',
-    Token.Menu.Completions.ProgressButton: 'bg:#003333',
-    Token.Menu.Completions.ProgressBar: 'bg:#00aaaa',
-    # toolbar
-    Token.Toolbar: '#ffffff bg:#333333',
-    # prompt
-    Token.Prompt:       '#444444',
-    Token.Continuation: '#333333',
-})
+def prepare_style(style):
+    style.styles.update({
+        # menu
+        Token.Menu.Completions.Completion.Current: 'bg:#00aaaa #000000',
+        Token.Menu.Completions.Completion: 'bg:#008888 #ffffff',
+        Token.Menu.Completions.ProgressButton: 'bg:#003333',
+        Token.Menu.Completions.ProgressBar: 'bg:#00aaaa',
+        # toolbar
+        Token.Toolbar: '#ffffff bg:#333333',
+        # prompt
+        Token.Prompt:       '#444444',
+        Token.Continuation: '#333333',
+    })
+    return style_from_pygments(style)
 
 def get_prompt_tokens(cli):
     return [ (Token.Prompt, '>>> ') ]
@@ -36,20 +38,21 @@ def get_prompt_tokens(cli):
 def get_continuation_tokens(cli, width):
     return [(Token.Continuation, '.' * (width - 1))]
 
-def repl(parser, interpreter):
+def repl(parser, interpreter, style_name='default'):
     print("Franz v0.0\n")
     history_file = os.path.join(str(Path.home()), '.franz-history')
     history = FileHistory(history_file)
     toolbar_value = 'Press [Alt+Enter] to evaluate an expression. [Ctrl+d] to exit. History saved in %s.' % history_file
+    style = prepare_style(repl_styles[style_name])
 
     while True:
         name_completer = WordCompleter(sorted(interpreter.context))
 
-        code = prompt (
+        code = prompt(
             multiline     = True,
             completer     = name_completer,
             history       = history,
-            style         = ReplStyle,
+            style         = style,
             mouse_support = False,
             lexer         = PygmentsLexer(FranzLexer),
             auto_suggest  = AutoSuggestFromHistory(),
@@ -62,16 +65,13 @@ def repl(parser, interpreter):
         )
 
         if not code.strip(): continue
-        if code == '\\v':
+        if code == '\\var':
             pprint(interpreter.context)
             continue
 
         try:
             ast = parser.parse(code)
-        except UnexpectedToken as e:
-            toolbar_value = str(e)
-            continue
-        except UnexpectedInput as e:
+        except (UnexpectedToken, UnexpectedInput) as e:
             toolbar_value = str(e)
             continue
 
--- lib/pygments.py
+++ lib/pygments.py
@@ -1,109 +1,19 @@
+import importlib
+
 from pygments.lexer import RegexLexer, bygroups
 from pygments.style import Style
+from pygments.styles import STYLE_MAP
 from pygments.token import *
 
-from .termcolors import termcolors
+def load_style(full_class_string):
+    modulename, styleclass = full_class_string.split('::')
+    module = importlib.import_module("pygments.styles." + modulename)
+    return getattr(module, styleclass)
 
-COMMENT = "%(Grey27)s" % termcolors
-KEYWORD = "%(PaleTurquoise4)s" % termcolors
-TYPE = "%(SlateBlue3)s" % termcolors
-LITERAL = "%(Grey85)s" % termcolors
-NAME = "%(Plum4)s italic" % termcolors
-FUNCTION = "%(SteelBlue)s italic" % termcolors
-FUNCTION_DEF = "%(SteelBlue3)s italic" % termcolors
-NUMBER = "%(Silver)s" % termcolors
-STRING = "%(Grey)s" % termcolors
-STRING_ESCAPE = "%(DarkRed)s" % termcolors
-STRING_INTERPOL = NAME
-OPERATOR = "%(Orange4)s" % termcolors
-OPERATOR_WORD = "%(Orange4)s" % termcolors
-PUNCTUATION = "%(Grey42)s" % termcolors
-BRACES = "%(Grey50)s" % termcolors
-TEXT = "%(Grey46)s" % termcolors
-
-class ReplStyle(Style):
-    styles = {
-        Comment: COMMENT,
-        Comment.Hashbang: '',
-        Comment.Multiline: '',
-        Comment.Preproc: '',
-        Comment.PreprocFile: '',
-        Comment.Single: COMMENT,
-        Comment.Special: '',
-        Error: '#960050 bg:#1e0010',
-        Escape: '',
-        Generic: '',
-        Generic.Deleted: '',
-        Generic.Emph: 'italic',
-        Generic.Error: '',
-        Generic.Heading: '',
-        Generic.Inserted: '',
-        Generic.Output: '',
-        Generic.Prompt: '',
-        Generic.Strong: 'bold',
-        Generic.Subheading: '',
-        Generic.Traceback: '',
-        Keyword: KEYWORD,
-        Keyword.Constant: '',
-        Keyword.Declaration: '',
-        Keyword.Namespace: '',
-        Keyword.Pseudo: '',
-        Keyword.Reserved: KEYWORD,
-        Keyword.Type: TYPE,
-        Literal: LITERAL,
-        Literal.Date: '',
-        Literal.Number: NUMBER,
-        Literal.Number.Bin: '',
-        Literal.Number.Float: '',
-        Literal.Number.Hex: '',
-        Literal.Number.Integer: '',
-        Literal.Number.Integer.Long: '',
-        Literal.Number.Oct: '',
-        Literal.String: STRING,
-        Literal.String.Affix: '',
-        Literal.String.Backtick: '',
-        Literal.String.Char: '',
-        Literal.String.Delimiter: '',
-        Literal.String.Doc: '',
-        Literal.String.Double: '',
-        Literal.String.Escape: STRING_ESCAPE,
-        Literal.String.Heredoc: '',
-        Literal.String.Interpol: STRING_INTERPOL,
-        Literal.String.Other: '',
-        Literal.String.Regex: '',
-        Literal.String.Single: '',
-        Literal.String.Symbol: '',
-        Name: NAME,
-        Name.Attribute: '',
-        Name.Builtin: '',
-        Name.Builtin.Pseudo: '',
-        Name.Class: '',
-        Name.Constant: '',
-        Name.Decorator: '',
-        Name.Entity: '',
-        Name.Exception: '',
-        Name.Function: FUNCTION,
-        Name.Function.Definition: FUNCTION_DEF,
-        Name.Function.Magic: '',
-        Name.Label: '',
-        Name.Namespace: '',
-        Name.Other: '',
-        Name.Property: '',
-        Name.Tag: '',
-        Name.Variable: '',
-        Name.Variable.Class: '',
-        Name.Variable.Global: '',
-        Name.Variable.Instance: '',
-        Name.Variable.Magic: '',
-        Operator: OPERATOR,
-        Operator.Word: OPERATOR_WORD,
-        Other: '',
-        Punctuation: PUNCTUATION,
-        Punctuation.Braces: BRACES,
-        String.Double: STRING,
-        Text: TEXT,
-        Text.Whitespace: ''
-    }
+# probably don't need to preload all styles...
+repl_styles = {}
+for name, import_info in STYLE_MAP.items():
+    repl_styles[name] = load_style(import_info)
 
 
 class FranzLexer(RegexLexer):
