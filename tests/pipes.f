95a460e487119c35e2b2108619f7aaeb758262d0
Cliff Wells <cwells@focusvision.com>
2017-08-23 19:29:13 -0700

    working on bug #1

--- tests/pipes.f
+++ tests/pipes.f
@@ -32,4 +32,25 @@ known  = [ 1, 16, 81, 256 ]
 try assert pipes == nested rescue errors += 1
 try assert nested == known rescue errors += 1
 
+# BUG #1
+# a = fn (n: int)
+#     for i in 1 to n {
+#         print("a.{i}");
+#         yield i;
+#     }
+
+# b = fn (iter: any)
+#     for i in iter {
+#         print("->b.{i}\n");
+#         yield i;
+#     }
+
+# c = fn (iter: any)
+#     for i in iter {
+#         print("->c.{i}\n");
+#         yield i;
+#     }
+
+# for i in c(b(a(5))) print("i={i}\n")
+
 print(if errors "Some tests failed ({errors}) errors\n" else "Passed with caveat: see source code of this test.\n")
